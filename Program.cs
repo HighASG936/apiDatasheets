dee96752d654b330d23bd42d4a5071fca41f5ff3
Aurelio Siordia <lz_rock@hotmail.com>
2025-01-22 21:54:54 -0600

    no swagger

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("DatasheetsConnec
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
