733ad780f8f79c668a0f656f8b4676946a2f6eeb
Aurelio Siordia <lz_rock@hotmail.com>
2025-01-22 22:28:19 -0600

    DefaultConnection

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataSheetsContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DatasheetsConnection")));
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DatasheetsConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
